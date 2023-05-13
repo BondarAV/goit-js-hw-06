a4f41c93c1b9f1601e0668fb3ff4587bb5cd2909
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-05-13 22:35:35 +0200

    Completes task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector("form.login-form");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  if (event.currentTarget.email.value === "" || event.currentTarget.password.value === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    const emailInput = event.currentTarget.elements.email.name;
+    const passwordInput = event.currentTarget.elements.password.name;
+    const emailValue = event.currentTarget.elements.email.value;
+    const passwordValue = event.currentTarget.elements.password.value;
+
+    const formData = {
+      [emailInput]: emailValue,
+      [passwordInput]: passwordValue
+    };
+    
+    console.log(formData);
+    
+    event.currentTarget.reset();
+  }
+});
+
 
