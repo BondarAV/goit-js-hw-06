cd9e0aafaa358c89f7058313b6edfe8f0a2b947f
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-05-14 01:17:12 +0200

    Completes task 9 + fixes css

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorChangeBtn = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+const colorOutput = document.querySelector("span.color");
+
+colorChangeBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = `${randomColor}`;
+  colorOutput.textContent = `${randomColor}`;
+});
